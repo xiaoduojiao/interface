60ccccb37cf6ca2d5270c85a190b1c2efc206f3c
xiaoduojiao487 <384526532@qq.com>
2024-03-20 23:59:35 +0800

    增加查询等待时间

--- until/db_until.py
+++ until/db_until.py
@@ -2,6 +2,30 @@ import time
 
 import pymysql
 
+def wait_and_check(func):
+    '''等待查询结果:超时时间自定义'''
+    def wrapper(*args,**kwargs):
+        try:
+            if kwargs.get('key') and kwargs.get('value') and kwargs.get('timeout'):
+                start = time.time()
+                result = func(*args, **kwargs)
+                while result[0][kwargs.get('key')] != kwargs.get('value'):
+                    if time.time() - start > kwargs['timeout']:
+                        print(f"查询超时，未查询到结果，期望为{kwargs.get('value')}，实际为{result[0][kwargs.get('key')] }:{kwargs.get('sql')}")
+                        break
+                    time.sleep(1)
+            elif kwargs.get('timeout') is not None:
+                start = time.time()
+                result = func(*args,**kwargs)
+                while len(result) == 0:
+                    if time.time() - start > kwargs['timeout']:
+                        print(f"查询超时，未查询到结果:{kwargs.get('sql')}")
+                        break
+                    time.sleep(1)
+            return func(*args, **kwargs)
+        except Exception as e:
+            return func(*args,**kwargs)
+    return wrapper
 
 class DBUtil:
 
@@ -14,7 +38,9 @@ class DBUtil:
             charset='utf8mb4',
             cursorclass=pymysql.cursors.DictCursor
         )
-    def select(self,sql):
+
+    @wait_and_check
+    def select(self, sql,timeout=1,key=None,value=None):
         cursor = self.connect.cursor()
         cursor.execute(sql)
         data = cursor.fetchall()
@@ -37,4 +63,6 @@ class DBUtil:
             self.connect.close()
 
 if __name__ == '__main__':
-    pass
+    db_util = DBUtil(host='xxx', user='xxx', password='xxx')
+    re = db_util.select(sql = "select * from xxx.xxx limit 1;",key='id',value=2,timeout=3)
+    print(re)
